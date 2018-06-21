64e4ada094f4c957c2e923f5adb5818f4c735f2f
ZHU <s_zhu@zenrin-datacom.net>
2018-06-21 10:51:47 +0900

    test

---
+++ src/main/java/com/mapcamp/MapcampApplication.java
@@ -0,0 +1,12 @@
+package com.mapcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapcampApplication.class, args);
+	}
+}
