68dd085a7c7ad0a45e4d52112503904233898865
KasuuuN <shidoan02@gmail.com>
2018-06-28 12:16:49 +0900

    user.java更新

---
+++ src/main/java/com/mapcamp/domain/entity/User.java
@@ -0,0 +1,104 @@
+package com.mapcamp.domain.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String image;
+	
+	@Column(nullable = false)
+	private Integer sex;
+	
+	@Column(nullable = false)
+	private Date birthday;
+
+	// ゲッターセッター
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void Image(String image) {
+		this.image = image;
+	}
+	
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+//	@OneToMany(mappedBy = "user")
+//	private List<Review> reviews;
+//
+//	public List<Review> getReviews() {
+//		return reviews;
+//	}
+//
+//	public void setReviews(List<Review> reviews) {
+//		this.reviews = reviews;
+//	}
+
+}
