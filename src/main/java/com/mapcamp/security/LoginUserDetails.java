766017a89eb188ae436e69cb1ecba92bd61d49ce
KasuuuN <shidoan02@gmail.com>
2018-07-18 12:32:28 +0900

    fileとname追加

--- src/main/java/com/mapcamp/security/LoginUserDetails.java
+++ src/main/java/com/mapcamp/security/LoginUserDetails.java
@@ -7,14 +7,26 @@ import com.mapcamp.domain.entity.User;
 public class LoginUserDetails extends org.springframework.security.core.userdetails.User{
 
     private Long userId;
+    private String profileImage;
+    private String name;
 
     public LoginUserDetails(User user) {
         super(user.getEmail(), user.getPassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
         this.userId = user.getId();
+        this.profileImage= user.getProfileImage();
+        this.name = user.getName();
+        
     }
 
     public Long getUserId() {
         return userId;
     }
+    
+    public String getProfileImage() {
+    	return profileImage;
+    }
 
+    public String getName() {
+    	return name;
+    }
 }
