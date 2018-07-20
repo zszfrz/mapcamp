7704b572fb3b3436450fa1834a4be5493fa7f6cf
Mayuka_Saito <mayuka020@gmail.com>
2018-07-20 12:05:54 +0900

    Post.javaからshopname削除

--- src/main/java/com/mapcamp/web/form/PostForm.java
+++ src/main/java/com/mapcamp/web/form/PostForm.java
@@ -10,8 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class PostForm {
 	
-	@NotBlank
-	private String shopname;
+	
 
 	private MultipartFile file;
 	
@@ -45,13 +44,6 @@ public class PostForm {
     private String category;
 	
 
-	
-	public String getShopname() {
-		  return shopname;
-	}
-	public void setShopname(String shopname) {
-		  this.shopname = shopname;
-	}
 	
 	public MultipartFile getFile() {
 	    return file;
